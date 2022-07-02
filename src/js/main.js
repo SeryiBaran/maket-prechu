812146d2d3fe9acf954a7e28ba4fd8d02e00eb95
SeryiBaran <seryibaranminepy@yandex.ru>
2022-07-02 20:39:27 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Импорты
-import Swiper, { Navigation, Manipulation } from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 import 'swiper/css';
 
 import '@fontsource/open-sans';
@@ -9,7 +9,7 @@ import '@fontsource/open-sans';
 import '../scss/standard.scss';
 import '../scss/style.scss';
 
-Swiper.use([Navigation, Manipulation]);
+Swiper.use([Navigation]);
 
 const swiper = new Swiper('.carousel', {
   navigation: {
@@ -17,18 +17,3 @@ const swiper = new Swiper('.carousel', {
     prevEl: '.carousel-button-prev',
   },
 });
-
-for (let _ = 0; _ < 3; _++) {
-  swiper.appendSlide(
-    `<div class="swiper-slide">
-      <div class="wrapper">
-        <img class="slide-img" src="./src/img/iphone_215.png" />
-        <div class="slide-text">
-          <h3>Lorem Ipsum is simply</h3>
-          <span>dummy text of the<br />printing & typesetting</span>
-          <a href="#!"><img src="./src/img/shape_2_84.png" /> Sign up now</a>
-        </div>
-      </div>
-    </div>`,
-  );
-}
