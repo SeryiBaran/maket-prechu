b597097ba1c096c234c3001fdcd8d7a50ba41e81
SeryiBaran <seryibaranminepy@yandex.ru>
2022-08-01 13:09:34 +0300

    Fonts fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,15 @@
 import Swiper, { Navigation } from 'swiper';
 import 'swiper/css';
 
-import '@fontsource/open-sans';
+// import '@fontsource/open-sans'; не работает
+
+const fonts = [
+  'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
+];
+
+fonts.forEach(font => {
+  document.head.innerHTML += `<style>@import url('${font}');</style>`;
+})
 
 import '../scss/standard.scss';
 import '../scss/style.scss';
